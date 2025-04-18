ee840b4eaf5eaf21c94d5b5fcf55acf5d049832c
daviddlv007 <davidlanzadlv7@gmail.com>
2025-04-18 15:49:17 -0400

    Restructuración de rutas y de modelo de solicitudes CRUD a la API REST

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'main_page.dart';
-import 'views/home_view.dart';
 import 'views/persona/persona_view.dart';
 // importa todas las vistas que necesites
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => MainPage(),
 
-  '/home': (context) => const HomeView(),
 
   '/personas': (context) => const PersonaView(),
 
@@ -15,7 +13,7 @@ final Map<String, WidgetBuilder> appRoutes = {
         appBar: AppBar(title: Text('Productos más vendidos')),
         body: Center(child: Text('Aquí van los productos más vendidos')),
       ),
-      
+
   '/descuentos': (context) => Scaffold(
         appBar: AppBar(title: Text('Descuentos')),
         body: Center(child: Text('Aquí van los descuentos disponibles')),
